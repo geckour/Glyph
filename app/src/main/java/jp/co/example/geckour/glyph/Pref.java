4baf185efc121e187e81aa4551a9c5e298e5a8e0
geckour <geckour@gmail.com>
2014-09-02 17:24:33 +0900

    Refactor directory name of java.

--- app/src/main/java/jp/co/example/geckour/glyph/Pref.java
+++
@@ -1,9 +0,0 @@
-package jp.co.example.geckour.glyph;
-
-import android.os.Bundle;
-
-public class Pref extends android.preference.PreferenceActivity {
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
-}
