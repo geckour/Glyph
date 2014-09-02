896b6f90d30a3601b7f58cfca485366f349e8318
geckour <geckour@gmail.com>
2014-09-03 01:41:18 +0900

    Add to git all of files.

---
+++ app/src/main/java/jp/org/geckour/glyph/Pref.java
@@ -0,0 +1,9 @@
+package jp.org.geckour.glyph;
+
+import android.os.Bundle;
+
+public class Pref extends android.preference.PreferenceActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+}
